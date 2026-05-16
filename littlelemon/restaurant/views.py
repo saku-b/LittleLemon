1c9a8d63ff5f35af0bada87da04a43ac648db80a
saku-b <113333218+saku-b@users.noreply.github.com>
2026-05-16 12:03:37 +0300

    static routes

---
+++ littlelemon/restaurant/views.py
@@ -0,0 +1,10 @@
+from urllib import request
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
